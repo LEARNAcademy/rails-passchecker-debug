3b71be944ffec4048555db94801a43824fca2b26
Damon Lemmons <damonlemmons@Damons-Air.localdomain>
2018-10-29 14:30:54 -0700

    errors included for debugging

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,13 @@
+
+class PasswordsController < ApplicationController
+    def check
+        userid = params[:userid]
+        password = params[:password]
+
+        if @userid != @password && @password =~ /\d/
+            flash.now[:notice] = "Credentials are acceptable."
+        else
+            flash.now[:notice] ="User ID and password cannot match and password must contain a number."
+        end
+
+end
